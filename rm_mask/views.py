a49d75a7e190d8413144f722ec65a37eab93e28e
hrbysnk <pepepor123@gmail.com>
2020-10-14 22:38:53 +0900

    Implement index page

--- rm_mask/views.py
+++ rm_mask/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+class IndexPageView(generic.TemplateView):
+    template_name = 'rm_mask/index.html'
