a084241801510ab0505dc0ac9f8bb64c95e0114a
r-akai <44189159+r-akai@users.noreply.github.com>
2020-10-17 16:27:28 +0900

    add estimate_mask_edge_positions

--- rm_mask/views.py
+++ rm_mask/views.py
@@ -5,6 +5,7 @@ from .forms import PhotoForm
 
 import os
 
+from .estimate_mask_edge_positions.estimate_mask_edge_positions import estimate_mask_edge_positions
 
 def IndexPageView(request):
     template_name = 'rm_mask/index.html'
@@ -23,16 +24,19 @@ def IndexPageView(request):
             form.save()
 
             input_image_path = os.path.join('media', form.instance.image.name)
-            edge_positions = [
-              {'x': 0.50, 'y': 0.40},
-              {'x': 0.30, 'y': 0.60},
-              {'x': 0.30, 'y': 0.75},
-              {'x': 0.35, 'y': 0.85},
-              {'x': 0.50, 'y': 0.90},
-              {'x': 0.65, 'y': 0.85},
-              {'x': 0.70, 'y': 0.75},
-              {'x': 0.70, 'y': 0.60},
-            ]
+            edge_positions = estimate_mask_edge_positions(input_image_path)
+
+            if edge_positions is None:
+                edge_positions = [
+                {'x': 0.50, 'y': 0.40},
+                {'x': 0.30, 'y': 0.60},
+                {'x': 0.30, 'y': 0.75},
+                {'x': 0.35, 'y': 0.85},
+                {'x': 0.50, 'y': 0.90},
+                {'x': 0.65, 'y': 0.85},
+                {'x': 0.70, 'y': 0.75},
+                {'x': 0.70, 'y': 0.60},
+                ]
 
             request.session['input_image_path'] = input_image_path
             request.session['edge_positions'] = edge_positions
